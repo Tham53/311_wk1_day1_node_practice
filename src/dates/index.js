044b4918ec74a191eb80c26b85708d078b555f20
THam53 <texas01988@gmail.com>
2020-07-12 20:00:18 -0500

    new

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,18 +1,35 @@
 // import moment here; use this package in each function
+const moment = require('moment');
+
 
 const today = () => {
-  // write code for dates.today
-
+  const days = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+  }
+  const intToday = moment().weekday()
+  const strToday = days[intToday]
+  return strToday
 }
 
 const calendar = () => {
-  // write code for dates.calendar
-
+  const formattedMonth = moment().format('MMM');
+  const formattedDay = moment().format('DD');
+  const formattedYear = moment().format('YYYY');
+  const wholeDate = `${formattedMonth} ${formattedDay}, ${formattedYear}`
+  // console.log(wholeDate)
+  return wholeDate
 }
 
+// calendar()
 const currentTime = () => {
-  // write code for dates.currentTime
-
+  const formattedTime = moment().format('LTS');
+  return formattedTime
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,28 @@
+//split
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
-
-const pairs = (str) => {
   // write code for strings.pairs
+const pairs = (str) => {
+  let strArr = Array.from(str)
 
+  let joinedArray = []
+  while(strArr.length > 0) {
+    console.log("COUNT")
+    let chunks = [strArr[0], strArr[1]]
+    strArr = strArr.slice(2, strArr.length)
+    let joinedChunks = chunks.join('')
+    joinedArray.push(joinedChunks)
+  }
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
 }
 
 module.exports = {
